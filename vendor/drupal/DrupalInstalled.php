ca291b0e9d11497bf9f1e3731f517651a18ece0d
vorvor <borisz.voros@cheppers.com>
2026-02-04 15:52:52 +0100

    kart subtheme added

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'c86b35d524673731';
+  public const string VERSIONS_HASH = '9ece40147c574d71';
 
 }
