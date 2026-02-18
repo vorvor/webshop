7a4e05583bbdec728487b12b6094ff7461d91ac5
vorvor <borisz.voros@cheppers.com>
2026-02-18 21:08:33 +0100

    lets make it live

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '49420540c6d0059f';
+  public const string VERSIONS_HASH = '01c006332a020775';
 
 }
